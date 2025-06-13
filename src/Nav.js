7fb19dacb02bc1234a113a25e9791ce4694df918
sunita baram <sunitabaram489@gmail.com>
2025-06-13 14:38:16 +0545

    Added responsive for ipad and mobile screen and hamburger meny

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import logo from './img/logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBagShopping } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+
 
 function Nav(){
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
             <header className = 'navBar'>
                 <img src = {logo}/>
-                <ul className = 'navItems'>
+                <ul id = 'navItems' className = {menuOpen ? 'show': ''}>
+                    <FontAwesomeIcon icon = {faXmark} id = "close" onClick = {() => setMenuOpen(false)}/>
                     <li><a href = '/' className = "active">Home</a></li>
                     <li><a href = '/'>Shop</a></li>
                     <li><a href = '/'>Blog</a></li>
@@ -14,6 +20,9 @@ function Nav(){
                     <li><a href = '/'>Contact</a></li>
                     <li><a href = '/'><FontAwesomeIcon icon = {faBagShopping} /></a></li>
                 </ul>
+                <div className = "hamburger">
+                    <FontAwesomeIcon icon = {faBars} id = "bar" onClick = {() => setMenuOpen(true)}/>
+                </div>
             </header>
     )
 }
